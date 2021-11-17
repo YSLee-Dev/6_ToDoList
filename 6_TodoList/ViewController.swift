6aad7c423de6183c1e974088d5c0c04f74952bbb
YSLee-Dev <iyunsu@iyunsuui-MacBookPro.local>
2021-11-17 18:48:42 +0900

    ToDo list app

---
+++ 6_TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6_TodoList
+//
+//  Created by 이윤수 on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
