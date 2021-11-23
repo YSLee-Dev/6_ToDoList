c5b92ed0672e091d56d622a7df54eb68f6b1d050
YSLee-Dev <ysleedev01@gmail.com>
2021-11-23 18:25:40 +0900

    add Btn 기능 추가

--- 6_TodoList/ViewController.swift
+++ 6_TodoList/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = .white
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: nil)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(tapEditBtn(sender:)))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapAddBtn(sender:)))
         
         self.tableView = UITableView()
         self.tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -27,11 +27,26 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             self.tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
-            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),  
             self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
+    
+    @objc func tapEditBtn(sender:UIBarButtonItem){
+        
+    }
 
-
+    @objc func tapAddBtn(sender:UIBarButtonItem){
+        let alert = UIAlertController(title: "할 일 추가", message: nil , preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "등록", style: .default){ _ in
+            print(alert.textFields?[0].text)
+        })
+        alert.addTextField(){ tf in
+            tf.placeholder = "할 일 입력"
+        }
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
 }
 
