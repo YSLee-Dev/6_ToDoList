ee964544a0e2d68422761b37723e0f12e6535313
YSLee-Dev <ysleedev01@gmail.com>
2021-11-25 18:45:25 +0900

    클로저 캡쳐 개념 학습 및 dequeueReusableCell 사용

---
+++ 6_TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  6_TodoList
+//
+//  Created by 이윤수 on 2021/11/23.
+//
+
+import Foundation
+
+struct Task {
+    var title : String // 할 일 내용
+    var done : Bool // 완료 여부
+}
--- 6_TodoList/ViewController.swift
+++ 6_TodoList/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var tableView : UITableView!
+    var tasks = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,8 @@ class ViewController: UIViewController {
         
         self.tableView = UITableView()
         self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView.dataSource = self
         self.view.addSubview(self.tableView)
         
         NSLayoutConstraint.activate([
@@ -38,8 +41,11 @@ class ViewController: UIViewController {
 
     @objc func tapAddBtn(sender:UIBarButtonItem){
         let alert = UIAlertController(title: "할 일 추가", message: nil , preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "등록", style: .default){ _ in
-            print(alert.textFields?[0].text)
+        alert.addAction(UIAlertAction(title: "등록", style: .default){ [weak self] _ in
+            guard let title = alert.textFields?[0].text else {return}
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
         })
         alert.addTextField(){ tf in
             tf.placeholder = "할 일 입력"
@@ -50,3 +56,15 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.tasks[indexPath.row].title
+        print(self.tasks[indexPath.row].title)
+        return cell
+    }
+}
